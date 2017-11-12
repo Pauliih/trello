9c59c048e021fca5320fc37a0857c144912c51f7
Pauliih <pau.campos89@gmail.com>
2017-11-12 15:45:50 -0300

    Version 0.0.3 check

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,17 +9,18 @@ function add(){
 	containerTarea = document.getElementById("containerTask");
 	containerTarea.setAttribute('style','display:none');
 	//containerTarea.classList.add("invisible");
-	
+	document.getElementById('addList').style.display = 'block';
+
 	//llamo elementos
 	var contTask = document.getElementById('containerTask');
-    var tasks = document.getElementById('tasks');
+    var tasks = document.getElementById('addList');
 
     //Creo elementos
 	var salto= document.createElement('br');
 	var newForm = document.createElement('form');
 	var task = document.createElement('input');
 	//Le doy atributos
-	task.setAttribute('placeholder','Añadir una lista...','id','task');
+	task.setAttribute('placeholder','Añadir una lista...');
 	task.setAttribute('id','task');
 	//creo elementos
 	var btnTask = document.createElement('button');
@@ -34,7 +35,7 @@ function add(){
 	newForm.appendChild(salto);
 	newForm.appendChild(btnTask);
 	//asigno hijos al padre de todo 
-	tasks.appendChild(newForm);
+	addList.appendChild(newForm);
 
 	/*_______________________________________________________________________________________
 	Version 0.0.2
@@ -59,9 +60,11 @@ function add(){
 		var txtTask = document.createTextNode(listName);
 		var addNewCard = document.createElement('a')
 		var txtNew = document.createTextNode("Añadir una tarjeta...");
+		/* Agregar un titulo que diga Nombre de la lista al div listBox con clase fullList*/
 		
 		pTxt.classList.add('listName');//Doy atributos
 		listBox.classList.add('fullList');
+		addNewCard.setAttribute('id','link');
 
 		//asigno hijos
 		containerTasks.appendChild(listBox);
@@ -70,7 +73,32 @@ function add(){
 		listBox.appendChild(addNewCard);
 		addNewCard.appendChild(txtNew);
 
-		//________________________________________________________________________________
-		
+		/*________________________________________________________________________________
+		Version 0.0.3
+		- Al dar click en "Añadir una tarea(tarjeta)", deberá mostrar un formulario con un 
+		textarea y un botón que diga "Añadir". */
+
+		addNewCard.addEventListener('click', function(){
+
+			var link = document.getElementById("link");
+			link.setAttribute('style','display:none');
+
+			var formCard = document.createElement('form');
+		   	var txtArea = document.createElement('textarea');
+		   	var btnAdd = document.createElement('button');
+   			var txtBtnAdd = document.createTextNode("Añadir");
+		    //atributos
+		    btnAdd.setAttribute("type", "button");
+		    btnAdd.classList.add("btnAdd");
+		   		
+		   	formCard.classList.add("form", "design");
+		   	txtArea.setAttribute("placeholder", "Añadir una tarjeta");
+				
+			pTxt.appendChild(txtArea);
+			formCard.appendChild(pTxt);
+			btnAdd.appendChild(txtBtnAdd);
+			formCard.appendChild(btnAdd);
+			listBox.appendChild(formCard);
+   		});
 	})
 }
